47225e3e4aa8868fc8d701cafbea6540ef9da7d6
Marc Eliot Stein <marc.eliot.stein@gmail.com>
2019-08-18 14:28:29 -0500

    work on home page mobile view

--- header.php
+++ header.php
@@ -26,9 +26,6 @@
 
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
-					<?php the_custom_logo(); ?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 				</div>
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
